94ce8227a8b617742d42e916e27d4adf94172c10
LvJianfeng <coderjianfeng@foxmail.com>
2016-08-11 13:53:55 +0800

    update nsdate

--- Foundation/NSDate/NSDate+Extension.swift
+++ Foundation/NSDate/NSDate+Extension.swift
@@ -49,6 +49,12 @@ extension NSDate{
         }
     }
     
+    public var weekDay: NSInteger{
+        get{
+            return weekDay(self)
+        }
+    }
+    
     //Private
     private func day(date: NSDate) -> NSInteger {
         let dayComponents = calendar.components(.Day, fromDate: date)
@@ -79,6 +85,11 @@ extension NSDate{
     private func second(date: NSDate) -> NSInteger {
         let dayComponents = calendar.components(.Second, fromDate: date)
         return dayComponents.second
+    }
+    
+    private func weekDay(date: NSDate) -> NSInteger{
+        let dayComponents = calendar.components(.Weekday, fromDate: date)
+        return dayComponents.weekday
     }
     
     //MARK:- 返回一年多少天
@@ -121,6 +132,122 @@ extension NSDate{
         return String.init(format: "%lu-%02lu-%02lu", date.year, date.month, date.day)
     }
     
-    //MARK:- 获取（年，月）内的第几周
+    //MARK:- 返回day天后的日期(若day为负数,则为|day|天前的日期)
+    //Public
+    func dateAfterMonth(month: NSInteger) -> NSDate {
+        return dateAfterDaysDate(self, month: month)
+    }
     
+    func dateAfterDay(day: NSInteger) -> NSDate {
+        return dateAfterDaysDate(self, day: day)
+    }
+    
+    //Private
+    private func dateAfterDaysDate(date: NSDate, month: NSInteger) -> NSDate{
+        let componentsToAdd = NSDateComponents()
+        componentsToAdd.month = month;
+        let dateAfterMonth = calendar.dateByAddingComponents(componentsToAdd, toDate: date, options:.WrapComponents)
+        return dateAfterMonth!
+    }
+    
+    private func dateAfterDaysDate(date: NSDate, day: NSInteger) -> NSDate{
+        let componentsToAdd = NSDateComponents()
+        componentsToAdd.day = day;
+        let dateAfterMonth = calendar.dateByAddingComponents(componentsToAdd, toDate: date, options:.WrapComponents)
+        return dateAfterMonth!
+    }
+    
+    
+    //MARK:- 获取该月的第一天的日期
+    //Public
+    public var beginDayOfMonth: NSDate{
+        get{
+            return beginDayOfMonth(self)
+        }
+    }
+    
+    //Private
+    private func beginDayOfMonth(date: NSDate) -> NSDate{
+        return dateAfterDaysDate(date, day: -date.day+1)
+    }
+    
+    //MARK:- 获取该月的最后一天的日期
+    //Public
+    public var lastDayOfMonth: NSDate{
+        get{
+            return lastDayOfMonth(self)
+        }
+    }
+    //Private
+    private func lastDayOfMonth(date: NSDate) -> NSDate{
+        let lastDate = beginDayOfMonth(date)
+        return lastDate.dateAfterMonth(0).dateAfterDay(-1)
+    }
+    
+    //MARK:- 获取星期几
+    //Public
+    public var weekString: String{
+        get{
+            return weekOfDay(self)
+        }
+    }
+    //Private
+    private func weekOfDay(date: NSDate) -> String{
+        switch date.weekDay{
+        case 1:
+            return "星期天";
+        case 2:
+            return "星期一";
+        case 3:
+            return "星期二";
+        case 4:
+            return "星期三";
+        case 5:
+            return "星期四";
+        case 6:
+            return "星期五";
+        case 7:
+            return "星期六";
+        default:
+            return "";
+        }
+    }
+    
+    //MARK:- 增加n天后的日期
+    func dateByAddingDays(days: NSInteger) -> NSDate{
+        let dateComponents = NSDateComponents()
+        dateComponents.day = days
+        let date = calendar.dateByAddingComponents(dateComponents, toDate: self, options: .WrapComponents)
+        return date!
+    }
+    
+    //MARK:- 获取指定月份的天数
+    //Public
+    func daysOfMonth(month: NSInteger) -> NSInteger {
+        return daysOfMonth(self, month: month)
+    }
+    
+    //Private
+    private func daysOfMonth(date: NSDate, month:NSInteger) -> NSInteger{
+        switch month {
+        case 1,3,5,7,8,10,12:
+            return 31
+        case 2:
+            return date.isLeapYear ? 29 : 28
+        default:
+            return 30
+        }
+    }
+    
+    //MARK:- 获取当前月份的天数
+    //Public
+    public var daysOfMonthInDate: NSInteger{
+        get{
+            return daysOfMonthInDate(self)
+        }
+    }
+    //Private
+    private func daysOfMonthInDate(date: NSDate) -> NSInteger{
+        return daysOfMonth(date, month: date.month)
+    }
 }
