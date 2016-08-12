0aedfa4f9ba15bf6f2ec95328a9448ddf5016b05
LvJianfeng <coderjianfeng@foxmail.com>
2016-08-12 09:56:27 +0800

    Add trims

--- Foundation/String/Trims.swift
+++ Foundation/String/Trims.swift
@@ -9,5 +9,43 @@
 import Foundation
 
 extension String{
+    //MARK:去除前后空格
+    func trimmingWhitespace() -> String {
+        return self.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceCharacterSet())
+    }
     
+    //MARK:去除字符串与空行
+    func trimmingWhitespaceAndNewlines() -> String {
+        return self.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceAndNewlineCharacterSet())
+    }
+    
+    //MARK:去除所有空格
+    mutating func trimmingAllWhitespace() -> String {
+        var tempArray = self.componentsSeparatedByCharactersInSet(NSCharacterSet.whitespaceCharacterSet())
+        tempArray = tempArray.filter{
+            $0 != ""
+        }
+        self = tempArray.joinWithSeparator("")
+        return self
+    }
+    
+    //MARK:去除所有空格及空行
+    mutating func trimmingAllWhitespaceAndNewlines() -> String {
+        var tempArray = self.componentsSeparatedByCharactersInSet(NSCharacterSet.whitespaceAndNewlineCharacterSet())
+        tempArray = tempArray.filter{
+            $0 != ""
+        }
+        self = tempArray.joinWithSeparator("")
+        return self
+    }
+    
+    //MARK:去除所有空格,使用自定义符号连接
+    mutating func trimmingAllWhitespaceJoinWithSymbol(symbol: String) -> String {
+        var tempArray = self.componentsSeparatedByCharactersInSet(NSCharacterSet.whitespaceCharacterSet())
+        tempArray = tempArray.filter{
+            $0 != ""
+        }
+        self = tempArray.joinWithSeparator(symbol)
+        return self
+    }
 }
